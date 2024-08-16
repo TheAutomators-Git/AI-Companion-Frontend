55618b2d96a46ad07990210d6e97c8b62a97b67d
TalalMajeed <m.talal.majeed@gmail.com>
2024-08-17 01:10:37 +0500

    Completed Introduction Page

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,32 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const Map<int, Color> color = {
+      50: Color.fromRGBO(255, 87, 87, .1),
+      100: Color.fromRGBO(255, 87, 87, .2),
+      200: Color.fromRGBO(255, 87, 87, .3),
+      300: Color.fromRGBO(255, 87, 87, .4),
+      400: Color.fromRGBO(255, 87, 87, .5),
+      500: Color.fromRGBO(255, 87, 87, .6),
+      600: Color.fromRGBO(255, 87, 87, .7),
+      700: Color.fromRGBO(255, 87, 87, .8),
+      800: Color.fromRGBO(255, 87, 87, .9),
+      900: Color.fromRGBO(255, 87, 87, 1),
+    };
+
+    MaterialColor customColor = const MaterialColor(0xFFFF5757, color);
+
     return MaterialApp(
-      title: 'Flutter App',
+      //Disable the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'AI Companion App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: customColor,
+        primaryColor: customColor,
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color(0xFFFF5757), brightness: Brightness.dark),
+        useMaterial3: true,
       ),
       initialRoute: '/',
       routes: {
--- lib/pages/introduction_page.dart
+++ lib/pages/introduction_page.dart
@@ -4,20 +4,48 @@ class IntroductionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Introduction'),
-      ),
-      body: Center(
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        width: double.infinity,
+        height: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Welcome to the Flutter App!'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-              child: Text('Get Started'),
+            Image.asset(
+              'lib/assets/logo.png',
+              height: 100,
+              width: 100,
+            ),
+            const SizedBox(height: 25),
+            const Text(
+              'Welcome to AI Companion!',
+              style: TextStyle(fontSize: 26, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 25),
+            const Text(
+              'An AI Based Companion App to generate pictures that stand out :)',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            SizedBox(
+              width: 240,
+              height: 45,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                ),
+                child: const Text(
+                  'Continue',
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
           ],
         ),
