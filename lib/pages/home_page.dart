bb1b4d39682002ad741278ccb53b2829b15f685b
Bilal Rana <bilalrana351>
2024-08-24 17:28:37 +0500

    Code to ping on /api/generate

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:typed_data';
 
 class HomePage extends StatefulWidget {
   final String apiUrl;
@@ -13,7 +14,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Map<String, String>> messages = [
+  List<Map<String, dynamic>> messages = [
     {
       "sender": "bot",
       "message": "Hello and welcome to Favlist, your Personalized AI Companion."
@@ -61,6 +62,54 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  void _saveConversation() async {
+    try {
+      var response = await http.post(
+        Uri.parse('${widget.apiUrl}/api/save'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(<String, dynamic>{
+          'name': 'User',
+          'id': 'AB123',
+        }),
+      );
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to save conversation');
+      }
+    } catch (e) {
+      print('Error saving conversation: $e');
+    }
+  }
+
+  void _saveImage() async {
+    try {
+      var response = await http.post(
+        Uri.parse('${widget.apiUrl}/api/generate'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body:
+            jsonEncode(<String, dynamic>{'name': 'Bilal Rana', 'id': 'ABC456'}),
+      );
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        Uint8List bytes = base64Decode(data['image']);
+        setState(() {
+          messages.add({"sender": "bot", "image": bytes});
+        });
+      } else {
+        throw Exception('Failed to load image');
+      }
+    } catch (e) {
+      setState(() {
+        messages.add({"sender": "bot", "message": "Something Went Wrong :("});
+      });
+    }
+  }
+
   void _pingAPI(String answer) async {
     setState(() {
       _isLoading = true;
@@ -72,20 +121,41 @@ class _HomePageState extends State<HomePage> {
           'Content-Type': 'application/json',
         },
         body: jsonEncode(<String, dynamic>{
-          'name': 'User',
+          'name': 'Bilal Rana',
           'answer': answer,
-          'id': 'AB123',
+          'id': 'ABC456',
           'topics': selectedItems.toList(),
         }),
       );
 
+      print(response.statusCode);
+
+      print(response.body);
+
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body);
+
+        // Check if the response contains a base64 image
+        if (data['completed'] == true) {
+          setState(() {
+            messages.add({
+              "sender": "bot",
+              "message":
+                  'Please wait while we generate your personalized image!'
+            });
+          });
+          _saveImage(); // Save the image that is generated
+        } else {
+          setState(() {
+            messages.add({"sender": "bot", "message": data['question']});
+          });
+        }
+
         setState(() {
-          messages.add({"sender": "bot", "message": data['question']});
           _isLoading = false;
         });
       } else {
+        _saveConversation(); // Save conversation even if the response fails
         throw Exception('Failed to load response');
       }
     } catch (e) {
@@ -105,9 +175,19 @@ class _HomePageState extends State<HomePage> {
       });
       _pingAPI(text);
     }
+    // _saveImage();
   }
 
-  Widget _buildMessage(Map<String, String> message, bool isLast) {
+  Widget _saveButton() {
+    return ElevatedButton(
+      onPressed: () {
+        _saveConversation();
+      },
+      child: const Text('Save Conversation'),
+    );
+  }
+
+  Widget _buildMessage(Map<String, dynamic> message, bool isLast) {
     if (message['message'] == '<SELECTOR />') {
       return _buildSelector();
     }
@@ -144,10 +224,15 @@ class _HomePageState extends State<HomePage> {
                         bottomRight: Radius.circular(10),
                       ),
               ),
-              child: Text(
-                message['message']!,
-                style: const TextStyle(color: Colors.white),
-              ),
+              child: message.containsKey('image')
+                  ? Image.memory(
+                      message['image'], // Display the image
+                      fit: BoxFit.cover,
+                    )
+                  : Text(
+                      message['message']!,
+                      style: const TextStyle(color: Colors.white),
+                    ),
             ),
           ),
         ),
@@ -281,75 +366,53 @@ class _HomePageState extends State<HomePage> {
                   style: TextStyle(fontSize: 26, color: Colors.white),
                   textAlign: TextAlign.left,
                 ),
-                SizedBox(
-                  width: 50,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () => {Navigator.pushNamed(context, '/login')},
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.all(12.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4),
-                      ),
-                    ),
-                    child: const Icon(Icons.arrow_back),
-                  ),
-                )
+                IconButton(
+                  onPressed: () {
+                    // Handle settings button press
+                  },
+                  icon: const Icon(Icons.settings, color: Colors.white),
+                ),
               ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
+            const SizedBox(height: 10),
             Expanded(
               child: ListView.builder(
-                reverse: true,
                 itemCount: messages.length,
                 itemBuilder: (context, index) {
-                  return _buildMessage(
-                      messages[messages.length - 1 - index], index == 0);
+                  bool isLast = index == messages.length - 1;
+                  return _buildMessage(messages[index], isLast);
                 },
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      controller: _controller,
-                      decoration: const InputDecoration(
-                        labelText: 'Type a message...',
-                        border: OutlineInputBorder(),
-                        filled: true,
-                        fillColor: Color.fromARGB(0, 255, 255, 255),
-                      ),
-                      enabled: !_isLoading,
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: _isLoading ? null : _sendMessage,
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.all(12.0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                      ),
-                      child: const Icon(Icons.send),
-                    ),
-                  )
-                ],
+            if (messages.isEmpty)
+              const Center(
+                child: CircularProgressIndicator(),
+              ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _controller,
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Type your message...',
+                hintStyle: const TextStyle(color: Colors.white54),
+                suffixIcon: IconButton(
+                  onPressed: _sendMessage,
+                  icon: const Icon(Icons.send, color: Colors.white),
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(255, 57, 56, 56),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
               ),
             ),
           ],
         ),
       ),
+      backgroundColor: const Color(0xFF232323),
     );
   }
 }
