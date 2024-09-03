5f744a99e5f92fa43c09ee4d03595b02990c0e6f
EznXadee <eznzaidi@gmail.com>
2024-09-04 01:01:45 +0500

    Display Page

--- lib/pages/categories_page.dart
+++ lib/pages/categories_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'display_page.dart'; // Import the DisplayPage
 
 class CategoriesPage extends StatefulWidget {
   @override
@@ -21,85 +22,84 @@ class _CategoriesPageState extends State<CategoriesPage> {
       appBar: AppBar(
         title: Text('Categories'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: GridView.builder(
-              padding: EdgeInsets.all(10.0),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 3 / 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if (selectedCategories.contains(index)) {
-                        selectedCategories.remove(index);
-                      } else {
-                        selectedCategories.add(index);
-                      }
-                    });
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: selectedCategories.contains(index)
-                          ? Colors.blueAccent
-                          : Colors.grey[300],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      categories[index],
-                      style: TextStyle(
-                        color: selectedCategories.contains(index)
-                            ? Colors.white
-                            : Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          double maxWidth = 600; // Set your desired maximum width here
+          double gridWidth = constraints.maxWidth < maxWidth
+              ? constraints.maxWidth
+              : maxWidth;
+
+          return Center(
+            child: SizedBox(
+              width: gridWidth,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: GridView.builder(
+                      padding: EdgeInsets.all(10.0),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 3 / 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
                       ),
+                      itemCount: categories.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              if (selectedCategories.contains(index)) {
+                                selectedCategories.remove(index);
+                              } else {
+                                selectedCategories.add(index);
+                              }
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: selectedCategories.contains(index)
+                                  ? Color(0xFFfc5656) // Updated color
+                                  : Colors.grey[300],
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            alignment: Alignment.center,
+                            child: Text(
+                              categories[index],
+                              style: TextStyle(
+                                color: selectedCategories.contains(index)
+                                    ? Colors.white
+                                    : Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
                     ),
                   ),
-                );
-              },
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ElevatedButton(
+                      onPressed: selectedCategories.length >= 5
+                          ? () {
+                              // Navigate to DisplayPage
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => DisplayPage(),
+                                ),
+                              );
+                            }
+                          : null,
+                      child: Text('Next'),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: selectedCategories.length >= 5
-                  ? () {
-                      // Navigate to the next page
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => NextPage()),
-                      );
-                    }
-                  : null,
-              child: Text('Next'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class NextPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Next Page'),
-      ),
-      body: Center(
-        child: Text(
-          'You have navigated to the next page!',
-          style: TextStyle(fontSize: 24),
-        ),
+          );
+        },
       ),
     );
   }
---
+++ lib/pages/display_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class DisplayPage extends StatefulWidget {
+  @override
+  _DisplayPageState createState() => _DisplayPageState();
+}
+
+class _DisplayPageState extends State<DisplayPage> {
+  int _currentIndex = 0;
+
+  final List<String> _imageUrls = [
+    'https://via.placeholder.com/600x400.png?text=Image+1',
+    'https://via.placeholder.com/600x400.png?text=Image+2',
+    'https://via.placeholder.com/600x400.png?text=Image+3',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Display Page'),
+      ),
+      body: Container(
+        color: Colors.white, // Ensure the background color is always white
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Heading
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Images',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            
+            // Image Carousel
+            Expanded(
+              child: Stack(
+                children: [
+                  CarouselSlider.builder(
+                    itemCount: _imageUrls.length,
+                    itemBuilder: (context, index, realIndex) {
+                      return Image.network(
+                        _imageUrls[index],
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      );
+                    },
+                    options: CarouselOptions(
+                      height: 400,
+                      viewportFraction: 1.0,
+                      initialPage: 0,
+                      enableInfiniteScroll: true,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          _currentIndex = index;
+                        });
+                      },
+                    ),
+                  ),
+                  // Left arrow
+                  Positioned(
+                    left: 8,
+                    top: 0,
+                    bottom: 0,
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back_ios, color: Colors.grey[700]),
+                      onPressed: () {
+                        setState(() {
+                          _currentIndex = (_currentIndex - 1 + _imageUrls.length) % _imageUrls.length;
+                        });
+                      },
+                    ),
+                  ),
+                  // Right arrow
+                  Positioned(
+                    right: 8,
+                    top: 0,
+                    bottom: 0,
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_forward_ios, color: Colors.grey[700]),
+                      onPressed: () {
+                        setState(() {
+                          _currentIndex = (_currentIndex + 1) % _imageUrls.length;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            
+            // Dot indicators
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _imageUrls.map((url) {
+                int index = _imageUrls.indexOf(url);
+                return Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _currentIndex == index
+                        ? Color(0xFFfc5656) // Replace blue with #fc5656
+                        : Colors.grey,
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
